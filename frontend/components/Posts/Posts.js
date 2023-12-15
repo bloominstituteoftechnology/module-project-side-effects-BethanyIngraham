47e93b0cc60912ef30b2feae28514bea66c0ca64
Bethany Ingraham <BethFIngraham@gmail.com>
2023-12-14 19:57:42 -0500

    Solutions for module proj

--- frontend/components/Posts/Posts.js
+++ frontend/components/Posts/Posts.js
@@ -4,10 +4,14 @@ import Post from './Post';
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
   const { likePost, posts } = props;
+  
 
   return (
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
+      {
+         posts.map(pst => <Post likePost={likePost} post={pst} key={pst.id}/>) //Posts takes in post with it's props
+      }
       {/* Check the implementation of Post to see what props it requires! */}
     </div>
   );
